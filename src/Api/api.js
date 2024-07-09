8854f09dceb24b407d75ccec7aa2b98795843cb6
Shivam Parkar <70848428+shivamparkar@users.noreply.github.com>
2024-07-09 06:15:05 +0530

    Updated api.js

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,10 +2,9 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const options = {
   method: 'GET',
   headers: {
-    'x-rapidapi-key': '405f9f8e89msh9e5d6ffb6a69bf9p12ef66jsn7165f37dd33e',
-    'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+    
   }
 };
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'; 
-export const WEATHER_API_KEY = '57c91b607dac36ab80799d08f0e7983d';
+export const WEATHER_API_KEY = '';
